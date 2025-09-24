3a9a1c70547db3a5adc1236285bc998a8c2e0469
bigfool <zarhloulyasser@gmail.com>
2025-09-24 06:14:18 +0100

    fix github pages routing - output to root dir

--- .eleventy.js
+++ .eleventy.js
@@ -14,7 +14,7 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site"
+      output: "."
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
